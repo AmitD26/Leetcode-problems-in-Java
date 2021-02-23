acdae6493fb814e006793865a8bf11d862ce9206
Amit Dharmadhikari <amitdharmadhikari41@gmail.com>
2021-02-23 12:55:28 -0800

    Two Sum added.

--- paypalInterview/src/paypalInterview/TwoSum.java
+++ paypalInterview/src/paypalInterview/TwoSum.java
@@ -1,5 +1,21 @@
 package paypalInterview;
 
-public class TwoSum {
+import java.util.HashMap;
 
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = null;
+    
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                ans = new int[] { map.get(temp), i };
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+    }
 }
+
